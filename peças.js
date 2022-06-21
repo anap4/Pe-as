7731517f1381c973d995af0f86ed0b32b5719c03
Ana Paula <anapcosta456@gmail.com>
2022-06-21 16:32:15 -0300

    first commit

---
+++ peças.js
@@ -0,0 +1,23 @@
+let pesoPeca = 125
+let numeroPecas = 12
+let nomePecas = "Rodas"
+
+if (pesoPeca > 100){
+console.log("Pode cadastrar a peça")
+
+} else{
+	console.log("Não é possível cadastrar a peça")
+}
+
+if (numeroPecas=12) {
+	console.log("Não é possível cadastrar a peça")
+} else{
+	console.log("Pode cadastrar a peça")
+}
+
+if (nomePecas.length > 3) {
+	console.log("Nome da peça inválido")
+} else{
+	console.log("Nome da peça válido")
+}
+
